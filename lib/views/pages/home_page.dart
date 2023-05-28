30ffa3a0d529cc893e39e0536f469e18099bf4aa
AYHSATHFAHMIDA <ayshafahmi26@gmail.com>
2023-05-28 21:22:40 +0530

    commit

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_app/views/widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,7 +16,7 @@ class _HomePageState extends State<HomePage> {
 
   final List<String> buttons=[
     'C','DEL','%','/',
-    '9','8','7','X',
+    '9','8','7','x',
     '6','5','4','-',
     '3','2','1','+',
     '0','.','ANS','=',
@@ -32,10 +33,15 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    SizedBox(
-                      height: 50,
-                    ),
+                    // SizedBox(
+                    //   height: 50,
+                    // ),
                     Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white60,
+                      ),
                       padding: EdgeInsets.all(20),
                       alignment: Alignment.centerLeft,
                         child: Text(
@@ -46,7 +52,12 @@ class _HomePageState extends State<HomePage> {
                         )
                     ),
                     Container(
+                      margin: EdgeInsets.all(10),
                       padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color:Colors.white60,
+                      ),
                       alignment: Alignment.centerRight,
                         child: Text(
                             userAnswer,
@@ -71,6 +82,7 @@ class _HomePageState extends State<HomePage> {
                         buttonTapped: (){
                           setState(() {
                             userQuestion='';
+                            userAnswer='';
                           });
                         },
                         buttonText: buttons[index],
@@ -83,11 +95,37 @@ class _HomePageState extends State<HomePage> {
                         buttonTapped: (){
                           setState(() {
                             userQuestion=userQuestion.substring(0,userQuestion.length-1);
+                            if(userQuestion.length==0){
+                              userAnswer='';
+                            }
                           });
                         },
                         buttonText: buttons[index],
                         color: Colors.red,
                         textColor: Colors.white,
+                      );
+                    }
+                    else if(index==buttons.length-1){
+                      return MyButtons(
+                        buttonTapped: (){
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blueGrey[700],
+                        textColor: Colors.white60,
+                      );
+                    }else if(index==buttons.length-2){
+                      return MyButtons(
+                        buttonTapped: (){
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.blueGrey[100],
+                        textColor: Colors.blueGrey[700],
                       );
                     }
                     else{
@@ -115,9 +153,23 @@ class _HomePageState extends State<HomePage> {
     );
   }
   bool isOperator(String x){
-    if(x=='%'||x=='/'||x=='X'||x=='-'||x=='+'||x=='='){
+    if(x=='%'||x=='/'||x=='x'||x=='-'||x=='+'||x=='='){
       return true;
     }
     return false;
+  }
+
+
+  void equalPressed(){
+    String finalQuestion=userQuestion;
+    finalQuestion=finalQuestion.replaceAll(RegExp('x'), '*');
+
+    Parser p=Parser();
+    Expression exp=p.parse(finalQuestion);
+    ContextModel cm=ContextModel();
+    double eval=exp.evaluate(EvaluationType.REAL, cm);
+
+    userAnswer=eval.toString();
+
   }
 }
